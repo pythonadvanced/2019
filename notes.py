8b8c3e7913de5060dc884211aba487062c048e27
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-10-17 12:08:32 +0200

    a notebook to gather ideas for larger scale problems also a pretext to start playing with jupytext-powered notebooks in .py format

---
+++ notes.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # idées de TP pour la suite
+
+# %% [markdown]
+# ## bdd
+#
+# vu dans les notebooks de Laurent Signac à ENSIP/Poitiers
+# <https://nbhosting.inria.fr/auditor/notebook/ensip:generic/diueil/1_2_2_Traitement_donnees_table-sujet>
+#
+# * exo sur une simulation de bdd en mémoire
+# * une table = une liste de dictionnaires
+# * sans doute améliorable en utilisant des itérateurs et générateurs pour les diverses opérations de combinaison
+
+# %% [markdown] {"slideshow": {"slide_type": "slide"}}
+# ## calculette infixe
+#
+# idée proposée par Aurélien Noce
