a034589bc36df254fd53c53802712b2f34133b18
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-01-14 21:52:22 +0100

    cours9

---
+++ cours9.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     formats: py:percent
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   notebookname: "tutorat sujet \xE9valuation"
+# ---
+
+# %% [markdown]
+# # cours ***Python avancé*** - semaine 9
+
+# %% [markdown]
+# unique thème du jour :
+#
+# * tutorat / préparation sur l'évaluation
+
+# %% [markdown]
+# ## énoncé de l'évaluation
+#
+# l'énoncé de l'évaluation se trouve ici  
+# <https://nbhosting.inria.fr/auditor/notebook/mines-python-advanced:advanced/sujet-evaluation.md>
+#
+
+# %% [markdown]
+# ***
+#
+# ## rappel - solutions
+#
+# lorsqu'elles sont publiées, les solutions se trouvent dans :
+# https://github.com/pythonadvanced/solutions
