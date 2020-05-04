cd4686a5f3fb76728c0d9d4d256765eab005e5e6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-05-04 16:51:21 +0200

    checkpoint skeleton for course extra2

---
+++ extra2.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     formats: py:percent
+#     notebook_metadata_filter: all,-language_info,-toc,-jupytext.text_representation.jupytext_version,-jupytext.text_representation.format_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   notebookname: programmation asynchrone
+#   version: '1.0'
+# ---
+
+# %% [markdown]
+# # cours ***Python avancé*** - 15 Mai 2020
+
+# %% [markdown]
+# Aujourd'hui nous abordons deux sujets qui n'ont rien à voir entre eux, mais qui peuvent tous les deux vous être utiles pour les projets :
+#
+# * bonnes pratiques pour organiser votre repo git
+# * notebooks interactifs
+#   
+# <span style="background-color:red; padding: 10px 30px;">
+#     notez bien la présence des quiz un peu plus bas dans ce document
+# </span>
+
+# %% [markdown]
+# ## struture type d'un repo git
+
+# %% [markdown]
+# ## notebooks interactifs
+
+# %% [markdown]
+# ## les quiz d'aujourd'hui
+
+# %%
+from nbautoeval import run_yaml_quiz
+
+# %% [markdown]
+# **Attention il y a deux quiz séparés**
+#
+# Évaluez les cellules suivantes pour voir le quiz, que nous vous demandons de remplir **le plus tôt possible après avoir suivi le cours**.
+#
+# <span style="background-color:red; padding: 5px; margin-top: 5px;">On relèvera les résultats le jour du cours à 20:00</span>
+
+# %% [markdown]
+# Rappel du mode d'emploi :
+#
+# * vous avez plusieurs essais pour y répondre, n'hésitez pas à revenir en arrière sur le cours en cas de besoin; vous avez le droit de faire tourner le code localement pour répondre
+# * toutes les questions ont au moins une réponse valable, si vous ne cochez aucune réponse on considère que vous préférez ne pas répondre
+# * le signe ♧ (c'est le cas pour toutes les questions ici) indique que plusieurs réponses sont possibles
+#
+#
+# * le barême est indiqué pour chaque question, par exemple `4 pts / -1 pt / 0 pt`  
+#   pour **bonne** réponse, **mauvaise** réponse, **pas de réponse**, respectivement
+# * votre score est calculé à chaque tentative, vous obtenez comme note finale **le meilleur** de ces scores  
+# * mais notez que le score est calculé comme un tout pour le quiz en entier; du coup ce n'est pas parce que vous avez répondu au moins une fois correctement à la question 1 que vous aurez les points correspondants
+# * vous aurez donc une note même si vous n'utilisez pas tous vos essais  
+#   il se peut que votre note soit supérieure au score final affiché, qui correspond à la dernière tentative
+
+# %% [markdown]
+# ### quiz - structure du repo git
+
+# %%
+run_yaml_quiz("quiz-extra2", "repo-git")
+
+# %% [markdown]
+# ### quiz - notebooks interactifs
+
+# %%
+run_yaml_quiz("quiz-extra2", "notebooks-interactifs")
+
+# %% [markdown]
+# ## si vous avez terminé
+
+# %% [markdown]
+# Si vous avez terminé de suivre le cours, que vous avez répondu au quiz, et que vous voulez approfondir, voici quelques idées pour explorer de votre côté :
+
+# %% [markdown]
+# ### exo -  niveau facile
